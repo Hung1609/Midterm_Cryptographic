4308cced9f012a3702ee2535bcfdef1a30057e47
Hung1609 <phanlachung2004@gmail.com>
2025-01-07 01:52:20 +0700

    update to encrypt pdf and png

--- src/decrypt_email.py
+++ src/decrypt_email.py
@@ -1,29 +1,31 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 import base64
+import os
 
-def decrypt(input="data/email_encrypted.txt",output="data/email_decrypted.txt",key="keys/aes_key.key"):
-    with open(key,"rb") as k:
-        key=k.read()
+def decrypt(input="data/email_encrypted.txt", output="data/email_decrypted.txt", key="keys/aes_key.key"):
+    with open(key, "rb") as k:
+        key = k.read()
         
-    with open(input,"r",encoding="utf-8") as i:
+    with open(input, "r", encoding="utf-8") as i:
         encoded_data = i.read()
-        
-    # convert data from base64 to binary
+    
     data = base64.b64decode(encoded_data)
     init_vector = data[:16]
     cipher_text = data[16:]
     
     cipher = AES.new(key, AES.MODE_CBC, iv=init_vector)
     
-    # decrypt and remove padding
-    plain_text_padded = cipher.decrypt(cipher_text)
-    plain_text = unpad(plain_text_padded, AES.block_size).decode("utf-8")
-    
-    # store result
-    with open(output, "w", encoding="utf-8") as o:
-        o.write(plain_text)
-        
-    print(f"Email decrypted successfully")
-    
+    plain_data_padded = cipher.decrypt(cipher_text)
+    try:
+        plain_data = unpad(plain_data_padded, AES.block_size).decode("utf-8")
+        with open(output, "w", encoding="utf-8") as o:
+            o.write(plain_data)
+        print(f"File decrypted successfully as text! Output saved to {output}")
+    except UnicodeDecodeError:
+        plain_data = unpad(plain_data_padded, AES.block_size)
+        with open(output, "wb") as o:
+            o.write(plain_data)
+        print(f"File decrypted successfully as binary! Output saved to {output}")
+
 decrypt()
--- src/encrypt_email.py
+++ src/encrypt_email.py
@@ -1,34 +1,37 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 import base64
+import os
 import math
 
-def email_size(data_size, block_size=16, base64_overhead=1.33):
-    padding = block_size - (data_size % block_size) if data_size % block_size != 0 else 0
-    encrypted_size = data_size + padding
-    return math.ceil(encrypted_size * base64_overhead)
+def email_size(file_path, block_size=AES.block_size, iv_size=16):
+    file_size = os.path.getsize(file_path)
+    padded_size = (file_size + block_size - 1) // block_size * block_size
+    encrypted_size = iv_size + padded_size
+    base64_size = (encrypted_size + 2) // 3*4
+    return base64_size
 
-def encrypt(input="data/email_plain.txt",output="data/email_encrypted.txt",key="keys/aes_key.key"):
+def encrypt(input="data/email_pdf.pdf",output="data/email_encrypted.txt",key="keys/aes_key.key"):
     with open(key,"rb") as k:
         key=k.read()
         
-    with open(input,"r",encoding="utf-8") as i:
-        plain_text = i.read()
+    with open(input,"rb") as i:
+        file_data = i.read()
         
+    #compute email size
+    estimated_email_size = email_size(input)
+    if estimated_email_size > 25 * 1024 * 1024: # = 25MB
+        print(f"The size of email is {estimated_email_size / (1024 * 1024):.2f} MB")
+        return   
+    
     cipher = AES.new(key, AES.MODE_CBC)
     ini_vector = cipher.iv
     
     # padding data
-    plain_text_padded = pad(plain_text.encode("utf-8"), AES.block_size)
-    
-    #compute email size
-    estimated_email_size = email_size(len(plain_text_padded))
-    if estimated_email_size > 25 * 1024 * 1024: # = 25MB
-        print(f"The size of email is too large!!!")
-        return 
+    file_data_padded = pad(file_data, AES.block_size)
     
     # encrypt data
-    cipher_text = cipher.encrypt(plain_text_padded)
+    cipher_text = cipher.encrypt(file_data_padded)
     result = base64.b64encode(ini_vector + cipher_text).decode("utf-8")
     
     # store result
